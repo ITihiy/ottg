d250d8c914db9cafed0d3af76c41a09a80b25d04
tihiy <alexanderser@yandex.ru>
2019-09-13 17:14:55 +0300

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,52 @@
 from django.test import TestCase
 
+from lists.models import Item
 
-class SmokeTest(TestCase):
-    def test_bad_math(self):
-        self.assertEqual(1 + 1, 3)
+
+class HomePageTest(TestCase):
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'lists/home.html')
+
+    def test_can_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('item 1', response.content.decode('utf-8'))
+        self.assertIn('item 2', response.content.decode('utf-8'))
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        self.assertEqual(saved_items[0].text, 'The first (ever) list item')
+        self.assertEqual(saved_items[1].text, 'Item the second')
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from lists.models import Item
+
+
+def home_page(request):
+    if request.method == 'POST':
+        item = Item(text=request.POST['item_text'])
+        item.save()
+        return redirect('/')
+    return render(request, 'lists/home.html', {'items': Item.objects.all()})
